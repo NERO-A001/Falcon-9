cde1ebb6522d966d36334b7cf6c50c260921e63e
Aayush <aayushmohan.a001@gmail.com>
2026-07-28 05:57:44 +0530

    Refined pitch maneuver

--- Falcon9.py
+++ Falcon9.py
@@ -30,21 +30,23 @@ Cd = 0.3 #drag coefficient
 dt = 0.5
 t_current = 0
 t_vertical = 10   # Time for straight vertical flight
-t_pitch_end = 45  # Time between this and vertical is the duration of pitch over
-theta = 0
-theta_target = np.deg2rad(45)
+kick_duration = 5
+kick_angle = np.deg2rad(20)
 height = 0
 x = 0
-vy = 0
-vx = 0
+v = 0
 current_mass = m0
+gamma = np.deg2rad(90)
+
+
+
 t_history = [t_current]
 height_history = [height]
 x_history = [x]
-vy_history = [vy]
-vx_history = [vx]
+v_history = [v]
 mass_history = [current_mass]
-S = (height, x, vy, vx, current_mass)
+gamma_history = [gamma]
+S = (height, x, v, current_mass, gamma)
 
 
 
@@ -84,12 +86,10 @@ def get_thrust(height,mass):
         mass_flow_rate = 0
     return(thrust, mass_flow_rate)
 
-def get_pitch_angle(t):   # angle is increasing linearly per second during the duration
-    if t_vertical <= t < t_pitch_end :
-        theta = theta_target * (t - t_vertical)/(t_pitch_end - t_vertical)
-    elif t >= t_pitch_end:
-        theta = theta_target    
-    else:
+def kick_phase(t):   
+    if t_vertical <= t < (t_vertical + kick_duration) :
+        theta = kick_angle* (t - t_vertical)/(kick_duration)
+    else: 
         theta = 0
     return(theta)
 
@@ -97,66 +97,66 @@ def get_pitch_angle(t):   # angle is increasing linearly per second during the d
 def derivative(S, t):
     height = S[0]
     x = S[1]
-    vy = S[2]
-    vx = S[3]
-    mass = S[4]
-    v = np.sqrt((vy**2) + (vx**2))
+    v = S[2]
+    mass = S[3]
+    gamma = S[4]
     density, pressure = get_air_density(height)
-    theta = get_pitch_angle(t)
+    theta = kick_phase(t)
     drag = 0.5 * density * (v**2) * A_e * Cd
-    if v == 0:
-        Dy = 0
-        Dx = 0
-    else:
-        Dy = -drag * (vy/v)
-        Dx = -drag * (vx/v)
     thrust, mass_flow_rate = get_thrust(height,mass)
-    Tx = thrust * np.sin(theta) 
-    Ty = thrust * np.cos(theta)
     gravity = get_gravity(height) * mass
-    Fy = Ty - (gravity + Dy)
-    Fx = Tx - Dx
-    ay = Fy/mass
-    ax = Fx/mass
-    dhdt = S[2]
-    dxdt = S[3]
-    dvydt = ay
-    dvxdt = ax
+    if t < t_vertical:
+        commanded_thrust_angle = np.deg2rad(90)
+    elif  t_vertical <= t < (t_vertical + kick_duration):
+        commanded_thrust_angle = np.deg2rad(90) - theta
+    else:
+        commanded_thrust_angle = gamma
+    mismatch = commanded_thrust_angle - gamma
+    F = (thrust * np.cos(mismatch)) - drag - (gravity * np.sin(gamma))
+    a = F/mass
+    dhdt = v * np.sin(gamma)
+    dxdt = v * np.cos(gamma)
+    dvdt = a
     dmdt = - mass_flow_rate
-    return(dhdt, dxdt, dvydt, dvxdt, dmdt)
+    if t < t_vertical:
+        dgdt = 0
+    elif  t_vertical <= t < (t_vertical + kick_duration):
+        dgdt = ((thrust * np.sin(mismatch)) - (gravity * np.cos(gamma)))/(v * mass)
+    else:
+        dgdt = ((thrust * np.sin(mismatch)) - (gravity * np.cos(gamma)))/(v * mass)
+    return(dhdt, dxdt, dvdt, dmdt, dgdt)
 
 while current_mass > dry_mass and t_current < 300:
-    k1h, k1x, k1vy, k1vx, k1m = derivative(S ,t_current)
-    S_temp = (height + k1h * dt/2, x + k1x * dt/2, vy + k1vy * dt/2, vx + k1vx * dt/2, current_mass + k1m * dt/2)
-    k2h, k2x, k2vy, k2vx, k2m = derivative(S_temp, t_current + dt/2)
-    S_temp2 = (height + k2h * dt/2, x + k2x * dt/2, vy + k2vy * dt/2,vx + k2vx * dt/2, current_mass + k2m * dt/2)
-    k3h, k3x, k3vy, k3vx, k3m = derivative(S_temp2, t_current+ dt/2)
-    S_temp3 = (height + k3h * dt, x + k3x * dt, vy + k3vy * dt, vx + k3vx * dt, current_mass + k3m * dt)
-    k4h, k4x, k4vy, k4vx, k4m = derivative(S_temp3, t_current + dt)
-
+    k1h, k1x, k1v, k1m, k1g = derivative(S ,t_current)
+    S_temp = (height + k1h * dt/2, x + k1x * dt/2, v + k1v * dt/2, current_mass + k1m * dt/2, gamma +k1g * dt/2)
+    k2h, k2x, k2v, k2m, k2g = derivative(S_temp, t_current + dt/2)
+    S_temp2 = (height + k2h * dt/2, x + k2x * dt/2, v + k2v * dt/2, current_mass + k2m * dt/2, gamma + k2g * dt/2)
+    k3h, k3x, k3v, k3m, k3g= derivative(S_temp2, t_current+ dt/2)
+    S_temp3 = (height + k3h * dt, x + k3x * dt, v + k3v * dt, current_mass + k3m * dt, gamma + k3g * dt)
+    k4h, k4x, k4v, k4m, k4g = derivative(S_temp3, t_current + dt)
     h_new = height + (dt/6) * (k1h + 2*k2h + 2*k3h + k4h)
     x_new = x + (dt/6) * (k1x + 2*k2x + 2*k3x + k4x)
-    vy_new = vy + (dt/6) * (k1vy + 2*k2vy + 2*k3vy+ k4vy)
-    vx_new = vx + (dt/6) * (k1vx + 2*k2vx + 2*k3vx+ k4vx)
+    v_new = v + (dt/6) * (k1v + 2*k2v + 2*k3v+ k4v)
     m_new = current_mass + (dt/6) * (k1m + 2*k2m + 2*k3m + k4m)
+    gamma_new = gamma + (dt/6) * (k1g + 2*k2g + 2*k3g + k4g)
 
     if m_new < dry_mass:
         m_new = dry_mass
 
     height = h_new
     x = x_new
-    vy = vy_new
-    vx = vx_new
+    v = v_new
     current_mass = m_new
-    S = (height, x, vy, vx, current_mass)
+    gamma = gamma_new
+    S = (height, x, v, current_mass, gamma)
     t_current = t_current + dt
 
     t_history.append(t_current)
     height_history.append(height)
     x_history.append(x)
-    vy_history.append(vy)
-    vx_history.append(vx)
+    v_history.append(v)
     mass_history.append(current_mass)
+    gamma_history.append(gamma)
 
 plt.style.use(['science', 'notebook', 'grid'])
 fig, axs = plt.subplots(3, 2, figsize=(10, 8))
@@ -164,15 +164,17 @@ fig, axs = plt.subplots(3, 2, figsize=(10, 8))
 t_array = np.array(t_history)
 height_array = np.array(height_history)
 x_array = np.array(x_history)
-vy_array = np.array(vy_history)
-vx_array = np.array(vx_history)
+v_array = np.array(v_history)
 mass_array = np.array(mass_history)
+gamma_array = np.array(gamma_history)
 
 maxh = np.argmax(height_array)
-minm = np.argmin(mass_array)
-maxvy = np.argmax(vy_array)
-maxvx = np.argmax(vx_array)
 xmax = np.argmax(x_array)
+maxv = np.argmax(v_array)
+minm = np.argmin(mass_array)
+ming = np.argmin(gamma_array)
+
+
 
 axs[0, 0].plot(t_array, mass_array)
 axs[0, 0].set_title("Mass")
@@ -190,26 +192,15 @@ axs[0,1].annotate(
     (t_array[maxh], height_array[maxh]),
     )
 
-axs[1, 0].plot(t_array, vy_array)
-axs[1, 0].set_title("Vertical Velocity")
-axs[1,0].scatter(t_array[maxvy], vy_array[maxvy], color = "red")
+axs[1, 0].plot(t_array, v_array)
+axs[1, 0].set_title("Velocity")
+axs[1,0].scatter(t_array[maxv], v_array[maxv], color = "red")
 axs[1,0].annotate(
-    f"Max Velocity: \n ({t_array[maxvy]:.2f}, {vy_array[maxvy]:.2f})",
-    (t_array[maxvy], vy_array[maxvy]),
+    f"Max Velocity: \n ({t_array[maxv]:.2f}, {v_array[maxv]:.2f})",
+    (t_array[maxv], v_array[maxv]),
     )
 
-axs[1, 1].plot(vx_array, vy_array)
-axs[1, 1].set_title("Velocity")
-axs[1,1].scatter(vx_array[maxvy], vy_array[maxvy], color = "red")
-axs[1,1].annotate(
-    f"Y max: \n ({vx_array[maxvy]:.2f}, {vy_array[maxvy]:.2f})",
-    (vx_array[maxvy], vy_array[maxvy]),
-    )
-axs[1,1].scatter(vx_array[maxvx], vy_array[maxvx], color = "red")
-axs[1,1].annotate(
-    f"X max: \n ({vx_array[maxvx]:.2f}, {vy_array[maxvx]:.2f})",
-    (vx_array[maxvx], vy_array[maxvx]),
-    )
+axs[1, 1].axis('off')
 
 axs[2, 0].plot(t_array, x_array)
 axs[2, 0].set_title("Range")
@@ -219,7 +210,13 @@ axs[2,0].annotate(
     (t_array[xmax], x_array[xmax]),
     )
 
-axs[2,1].axis('off')
+axs[2, 1].plot(t_array, gamma_array)
+axs[2, 1].set_title("gamma")
+axs[2,1].scatter(t_array[ming], gamma_array[ming], color = "red")
+axs[2,1].annotate(
+    f"Max Range: \n ({t_array[ming]:.2f}, {gamma_array[ming]:.2f})",
+    (t_array[ming], gamma_array[ming]),
+)
 
 
 plt.tight_layout()
